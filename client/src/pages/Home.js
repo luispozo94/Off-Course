1031f7c8d4e43929d2072cde61129e007dc69ec2
ryanramb <ryanrambaran.fl@gmail.com>
2021-09-25 18:10:15 -0400

    attempt1

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,43 @@
-import React from "react"
+import React, { useState } from 'react';
+import '../styles/style.css';
+import sliderImg from '../assets/images/homepage.jpeg';
+import sliderImg2 from '../assets/images/homepage2.jpeg';
+import sliderImg3 from '../assets/images/homepage3.jpeg';
 
-import "../styles/style.css"
-import testImg from "../assets/images/tester.jpg"
+const images = [sliderImg, sliderImg2, sliderImg3];
 
 export default function Home() {
-    return (
-      <React.Fragment>
-        <header className="header__test">
-            <h1>React Boilerplate</h1>
-            <p>A webpack bundled project featuring Babel & React</p>
-            <img className="header__image" src={testImg}></img>
-        </header>
-        <section>
-            more content
-        </section>
-      </React.Fragment>  
-    );
+  // creating state variables
+  // currentImage stores current [index] of poistion in images arr
+  // setCurrentImage function to update currentImage
+  // useState argument is the initial value for currentImage -> [0]
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const handleClick = () => {
+    const length = images.length - 1;
+
+    setCurrentImage((currentImage) => {
+      if (currentImage < length) {
+        return currentImage + 1;
+      } else {
+        return 0;
+      }
+    });
+  };
+
+  return (
+    <div className="fullpage">
+      <div className="title">
+        <h1>React Boilerplate</h1>
+        <p>A webpack bundled project featuring Babel & React</p>
+      </div>
+      <div className="image_container">
+        <img
+          className="image_slider"
+          src={images[currentImage]}
+          onClick={handleClick}
+        ></img>
+      </div>
+    </div>
+  );
 }
